0a49ba96d00ad68a181de881eee4ceef492a1f16
Adam Eliezerov <adaminspaceship@gmail.com>
2018-09-19 23:35:32 +0300

    sort by recent

--- Polocal/MainViewController.swift
+++ Polocal/MainViewController.swift
@@ -39,12 +39,13 @@ class MainViewController: UIViewController {
                 let falseAnswers = json["answers"]["false"].intValue
                 let trueAnswers = json["answers"]["true"].intValue
                 self.Posts.append(Post(question: question, falseAnswers: falseAnswers, trueAnswers: trueAnswers, postID: rest.key))
-                let currentPost = self.Posts[self.postCount]
-                self.questionLabel.text = currentPost.question
+				
             }
+			self.postCount = self.Posts.count-1
+			let currentPost = self.Posts[self.postCount]
+			self.questionLabel.text = currentPost.question
         }
-        
-        
+		
     }
 
     override func didReceiveMemoryWarning() {
@@ -63,13 +64,12 @@ class MainViewController: UIViewController {
     }
     */
 
-    @objc func readNewPost() {
-        postCount += 1
+	func readNewPost() {
+        postCount -= 1
         let currentPost = Posts[postCount]
         questionLabel.text = currentPost.question
         truePercentageLabel.isHidden = true
         falsePercentageLabel.isHidden = true
-        
     }
     
     @IBAction func falseAnswerButtonTapped(_ sender: Any) {
@@ -85,22 +85,7 @@ class MainViewController: UIViewController {
         truePercentageLabel.isHidden = false
         truePercentageLabel.text = "\(String(truePercentage))%"
         falsePercentageLabel.text = "\(String(falsePercentage))%"
-        if falsePercentage>truePercentage {
-            UIView.animate(withDuration: 1) {
-                let num = 319/(100/Double(falsePercentage))
-                self.falseView.frame.size.width = CGFloat(num)
-            }
-        }else {
-            UIView.animate(withDuration: 1) {
-                let num = 319/(100/Double(truePercentage))
-                self.trueView.isHidden = false
-                for i in 0...Int(num) {
-                    self.trueView.frame.size.width += 1
-                    self.trueView.center = CGPoint(x: self.trueView.center.x-1, y: self.trueView.center.y)
-                }
-            }
-        }
-        
+        showPercentage(falsePercentage: falsePercentage, truePercentage: truePercentage)
     }
     
     var timer = Timer()
@@ -117,41 +102,58 @@ class MainViewController: UIViewController {
         let truePercentage = Int(100-falsePercentage)
         truePercentageLabel.isHidden = false
         falsePercentageLabel.isHidden = false
-        
         truePercentageLabel.text = "\(String(truePercentage))%"
         falsePercentageLabel.text = "\(String(falsePercentage))%"
-        if falsePercentage>truePercentage {
-            let num = 319/(100/Double(falsePercentage))
-            UIView.animate(withDuration: 1, animations: {
-                self.falseView.frame.size.width = CGFloat(num)
-            }) { (complete) in
-                self.timer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(self.readNewPost), userInfo: nil, repeats: false)
-                UIView.animate(withDuration: 1, animations: {
-                    for i in 0...Int(num) {
-                        self.falseView.frame.size.width = 0
-                    }
-                })
-                
-            }
-        }else {
-            let num = 319/(100/Double(truePercentage))
-            self.trueView.isHidden = false
-            UIView.animate(withDuration: 1, animations: {
-                for i in 0...Int(num) {
-                    self.trueView.frame.size.width += 1
-                    self.trueView.center = CGPoint(x: self.trueView.center.x-1, y: self.trueView.center.y)
-                }
-            }) { (complete) in
-                UIView.animate(withDuration: 1, animations: {
-                    for i in 0...Int(num) {
-                        self.trueView.frame.size.width -= 1
-                        self.trueView.center = CGPoint(x: self.trueView.center.x+1, y: self.trueView.center.y)
-                    }
-                })
-                self.timer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(self.readNewPost), userInfo: nil, repeats: false)
-            }
-        }
+		showPercentage(falsePercentage: falsePercentage, truePercentage: truePercentage)
     }
+	
+	
+	
+	
+	
+	
+	
+	
+	func showPercentage(falsePercentage: Int, truePercentage: Int) {
+		if falsePercentage>truePercentage {
+			let num = 319/(100/Double(falsePercentage))
+			UIView.animate(withDuration: 2, delay: 0, options: .curveEaseIn, animations: {
+				self.falseView.frame.size.width = CGFloat(num)
+			}) { (complete) in
+				sleep(2)
+				UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut, animations: {
+					for _ in 0...Int(num) {
+						self.falseView.frame.size.width = 0
+					}
+				}) { (complete) in
+					self.readNewPost()
+//				self.timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(self.readNewPost), userInfo: nil, repeats: false)
+				}
+				
+			}
+		}else {
+			let num = 319/(100/Double(truePercentage))
+			self.trueView.isHidden = false
+			UIView.animate(withDuration: 2, delay: 0, options: .curveEaseIn, animations: {
+				for _ in 0...Int(num) {
+					self.trueView.frame.size.width += 1
+					self.trueView.center = CGPoint(x: self.trueView.center.x-1, y: self.trueView.center.y)
+				}
+			}) { (complete) in
+				sleep(2)
+				UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut, animations: {
+					for _ in 0...Int(num) {
+						self.trueView.frame.size.width -= 1
+						self.trueView.center = CGPoint(x: self.trueView.center.x+1, y: self.trueView.center.y)
+					}
+				}){ (complete) in
+					self.readNewPost()
+//				self.timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(self.readNewPost), userInfo: nil, repeats: false)
+				}
+				
+			}
+		}
+	}
     
 }
 
