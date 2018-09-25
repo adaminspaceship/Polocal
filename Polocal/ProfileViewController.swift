fe9880679f70385466f8bf1817247f419be78944
Labby Lab <instagrambetatesting@gmail.com>
2018-09-25 23:37:40 +0300

    custom cell works

---
+++ Polocal/ProfileViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ProfileViewController.swift
+//  Polocal
+//
+//  Created by Adam Eliezerov on 25/09/2018.
+//  Copyright © 2018 Adam Eliezerov. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "questionCell") as! QuestionTableViewCell
+        cell.questionLabel.text = "fa"
+        return cell
+    }
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Polocal/QuestionTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  QuestionTableViewCell.swift
+//  Polocal
+//
+//  Created by Adam Eliezerov on 25/09/2018.
+//  Copyright © 2018 Adam Eliezerov. All rights reserved.
+//
+
+import UIKit
+
+class QuestionTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
